51052c1ae6dda20e0d77726444a2c27e65a2f88f
misc <hunsi250@users.noreply.github.com>
2026-08-22 20:59:52 +0000

    ehentai 日语+排除列表精简到11种(ehentai语言下拉确定值), 去掉无效值 arabic/latin/indonesian/polish/dutch/turkish

--- ehentai_hermes.js
+++ ehentai_hermes.js
@@ -45,7 +45,7 @@ class Ehentai extends ComicSource {
     // unique id of the source
     key = "ehentai_hermes"
 
-    version = "1.2.20"
+    version = "1.2.21"
 
     minAppVersion = "1.5.3"
 
@@ -560,7 +560,7 @@ class Ehentai extends ComicSource {
             const LOOSE = "japanese_loose";
             if (langs.includes(LOOSE)) {
                 if (!keyword.includes("-language:")) {
-                    keyword += ` -language:english -language:chinese -language:korean -language:french -language:german -language:spanish -language:italian -language:portuguese -language:russian -language:thai -language:vietnamese -language:indonesian -language:polish -language:dutch -language:turkish -language:arabic -language:latin`;
+                    keyword += ` -language:english -language:chinese -language:korean -language:french -language:german -language:spanish -language:italian -language:portuguese -language:russian -language:thai -language:vietnamese`;
                 }
             } else if (langs.length && !keyword.includes("language:")) {
                 keyword += ` ${langs.map(l => `language:${l}`).join("$")}`;
